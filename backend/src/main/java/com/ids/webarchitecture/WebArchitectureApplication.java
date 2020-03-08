23550b5fd15012c47144ec3e506ce12a68468be2
Denis <iv21dk@gmail.com>
2020-03-08 11:26:12 +0300

    ajshjasd

---
+++ backend/src/main/java/com/ids/webarchitecture/WebArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.ids.webarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebArchitectureApplication.class, args);
+	}
+
+}
