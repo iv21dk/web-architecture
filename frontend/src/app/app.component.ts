404523c593fb5836aac300e12f0a08bc70414822
Denis <iv21dk@gmail.com>
2020-03-16 00:19:27 +0300

    Global restructuring of main page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'web-architecture';
+  selectedSection: string;
+
+  ngOnInit(): void {
+    this.selectedSection = 'TESTS';
+  }
+
+  selectSection(selectedSection: string) {
+    this.selectedSection = selectedSection;
+  }
+
+  isSelected(section: string): boolean {
+    return section === this.selectedSection;
+  }
+
 }
